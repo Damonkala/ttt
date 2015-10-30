3a4fbb1be04024146dd597baecd2974846834862
Damon Kalaboukis <damonkala@yahoo.com>
2015-10-29 17:59:17 -0700

    Allow for clicking the boxes and guard for reclicking

---
+++ main.js
@@ -0,0 +1,28 @@
+
+(function app() {
+	'use strict';
+
+	let documentReady = () => {
+		let lastMark;
+
+		$('.box').on("click.mark", (event) => {
+			let $e = $(event.target);
+
+			if( $e.data("clicked")){
+				return;
+			}
+
+			lastMark = (lastMark === "X") ? "O" : "X";
+
+			$e.text(lastMark);
+
+		//	$e.off("click.mark");
+
+			$e.data("clicked", true);
+
+		});
+	}
+
+	$(documentReady);
+
+})();
